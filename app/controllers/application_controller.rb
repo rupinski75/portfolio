74b57a61cadae9fdaadc29333e4428f510c44972
rupinski75 <rupinski.k@gmail.com>
2019-02-03 10:47:21 +0100

    mostly css and content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_perrmited_parameters, if: :devise_controller?
+
+  def configure_perrmited_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :trackable
   enum user_type: { user: 0, admin: 1}
-  enum is_confirmed: { not_confirmed: 0, confirmed: 1}
+  #enum is_confirmed: { not_confirmed: 0, confirmed: 1}
   validates_presence_of :email, :password
   has_many :comments
   has_many :galleries
