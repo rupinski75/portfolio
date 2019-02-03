cad70d16597da60b02dec48a5158290e52f4aeb0
rupinski75 <rupinski.k@gmail.com>
2019-02-03 02:07:29 +0100

    Integrated new and create actions for gallery

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -8,8 +8,28 @@ class GalleriesController < ApplicationController
   def show
   end
 
-  private
-  def set_gallery
-    @gallery = Gallery.find(params[:id])
+  def new
+    @gallery = Gallery.new
   end
+
+  def create
+    @gallery = Gallery.new(gallery_params)
+
+    respond_to do |format|
+      if @gallery.save
+        format.html { redirect_to galleries_path, notice: 'Gallery was created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+    def set_gallery
+      @gallery = Gallery.find(params[:id])
+    end
+
+    def gallery_params
+      params.require(:gallery).permit(:title, :description, :thumb_image, :user_id)
+    end
 end
