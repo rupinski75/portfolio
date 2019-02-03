6f6a289a89a45658549bb95715b324d2b0d5fcb1
rupinski75 <rupinski.k@gmail.com>
2019-02-03 09:12:17 +0100

    auth finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable, :trackable
   enum user_type: { user: 0, admin: 1}
   enum is_confirmed: { not_confirmed: 0, confirmed: 1}
-  validates_presence_of :email, :password, :name
+  validates_presence_of :email, :password
   has_many :comments
   has_many :galleries
   has_many :articles
