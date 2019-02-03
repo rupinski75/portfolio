128df33becdac7cdadd76488957643ad9a8d1e07
rupinski75 <rupinski.k@gmail.com>
2019-02-03 03:21:35 +0100

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
   resources :users
   resources :galleries
   resources :articles
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  root 'pages#home'
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
